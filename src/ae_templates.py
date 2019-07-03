ca08ac02173bc869ceecbecb2d53585b9fbe59dd
MiaoDragon <innocentmyl@gmail.com>
2019-07-03 14:59:07 -0700

    can now use number of points other than 2048

--- src/ae_templates.py
+++ src/ae_templates.py
@@ -11,8 +11,8 @@ from . encoders_decoders import encoder_with_convs_and_symmetry, decoder_with_fc
 def mlp_architecture_ala_iclr_18(n_pc_points, bneck_size, bneck_post_mlp=False):
     ''' Single class experiments.
     '''
-    if n_pc_points != 2048:
-        raise ValueError()
+    #if n_pc_points != 2048:
+    #    raise ValueError()
 
     encoder = encoder_with_convs_and_symmetry
     decoder = decoder_with_fc_only
